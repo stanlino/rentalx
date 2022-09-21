2e1d81796f043cbdd5033ee894a45d6fec6dbf09
Stanley Filipe <stanley92filipe@gmail.com>
2022-09-21 09:41:43 -0300

    feat: admin middleware

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -2,10 +2,18 @@ import { Router } from 'express';
 
 import { CreateCarControler } from '@modules/cars/useCases/createCar/CreateCarController';
 
+import { ensureAdmin } from '../middlewares/ensureAdmin';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+
 const carsRoutes = Router();
 
 const createCarController = new CreateCarControler();
 
-carsRoutes.post('/', createCarController.handle);
+carsRoutes.post(
+  '/',
+  ensureAuthenticated,
+  ensureAdmin,
+  createCarController.handle,
+);
 
 export { carsRoutes };
