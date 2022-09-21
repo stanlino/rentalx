42c3cbe61b3594927c5708ccb612f2ffce9498d8
Stanley Filipe <stanley92filipe@gmail.com>
2022-09-21 09:30:35 -0300

    feat: seed user

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -5,11 +5,13 @@ import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
 import '../../container';
-import '../typeorm';
 
 import swaggerFile from '../../../swagger.json';
 import { AppError } from '../../errors/AppError';
+import createConnection from '../typeorm';
 import { router } from './routes';
+
+createConnection();
 
 const app = express();
 
