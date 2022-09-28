951ec511a8317726ea5e8595d76c0f9af4325984
Stanley Filipe <stanley92filipe@gmail.com>
2022-09-28 03:05:09 -0300

    refactor: user auth

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: '9a4111a7ffd4c2fc43730105eb169f5c',
+  expires_in_token: '15m',
+  secret_refresh_token: '6620faa74e67d267be6b72421b3cbf5b',
+  expires_in_refresh_token: '30d',
+  expires_refresh_token_days: 30,
+};
